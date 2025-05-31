edf52d9a056ec6cef577f795d6dea9f0192f252c
Yu Jin <qldls0307@naver.com>
2025-06-01 02:06:07 +0900

    add: queue, stack 시간초과

---
+++ algorithms/data_structure/jin/10828_stack.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+n = int(input())
+q = deque()
+
+for i in range(n):
+  a = input().split()
+  if a[0] == 'push':
+    # print(a[1])
+    q.append(int(a[1]))
+  elif a[0] == 'pop':
+    if len(q) == 0:
+      print(-1)
+    else:
+      last = len(q)
+      print(q[last - 1])
+      q.pop()
+  elif a[0] == 'size':
+    print(len(q))
+  elif a[0] == 'empty':
+    if len(q) == 0:
+      print(1)
+    else:
+      print(0)
+  elif a[0] == 'top':
+    if len(q) == 0:
+      print(-1)
+    else:
+      last = len(q)
+      print(q[last - 1])
