6b41c68554678396c3d11a057cdfb582e63b8248
Yu Jin <qldls0307@naver.com>
2025-06-06 00:39:07 +0900

    add: 11728 배열합치기

---
+++ algorithms/two_pointer/jin/11728.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+c = a + b
+
+c.sort()
+
+# print(c)
+
+for i in c:
+  print(i, end=" ")
